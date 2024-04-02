f781e4125057b0bf13b280a78b8c8926cc50d289
Анна Мизропова <anmizrop@gmail.com>
2024-04-02 15:16:16 +0300

    Добавьте файлы проекта.

---
+++ TaskScheduler/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TaskScheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить задачу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить задачу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(345, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Планироващик задач";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(655, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Действия";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(633, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(551, 96);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 19);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Сделано";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(598, 130);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 56);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(520, 378);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = ":)";
+            this.columnHeader1.Width = 65;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Имя";
+            this.columnHeader2.Width = 151;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Состояние";
+            this.columnHeader3.Width = 151;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Дата";
+            this.columnHeader4.Width = 151;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(648, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 128);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Чтобы удалить задачу, нажмите на область в котором находится квадрат(CheckBox, пу" +
+    "стая область справа от него), означающий выполнена ли задачу или нет";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(648, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 126);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Чтобы добавить задачу, напиши в строке(TextBox) название задачу, дату её дедлайна" +
+    ", поставьте галочку если она уже сделана, в обратном случае не ставьте";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 455);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TaskScheduler/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskScheduler
+{
+    public partial class Form1 : Form
+    {
+        int windth = 830;
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.CheckBoxes = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            out_file();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            Pen blackPen = new Pen(Color.Black, 2);
+            g.DrawLine(blackPen, 0, 25, windth, 25);
+            blackPen.Dispose();
+            g.Dispose();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                ListViewItem item = new ListViewItem();
+                string date = dateTimePicker1.Value.ToString();
+                if (checkBox1.Checked == true)
+                {
+                    item.Checked = true;
+                    item.SubItems.Add(textBox1.Text);
+                    item.SubItems.Add("Сделано");
+                    item.BackColor = Color.Blue;
+                }
+                else if (DateTime.Now > dateTimePicker1.Value)
+                {
+                    item.SubItems.Add(textBox1.Text);
+                    item.SubItems.Add("Просроченно");
+                    item.BackColor = Color.Red;
+                }
+                else if (DateTime.Now <= dateTimePicker1.Value)
+                { 
+                    item.SubItems.Add(textBox1.Text);
+                    item.SubItems.Add("В процессе");
+                    item.BackColor = Color.LightGray;
+                }
+                item.SubItems.Add(date);
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem item2 in listView1.Items)
+            {
+                if (item2.Selected)
+                    item2.Remove();
+            }
+        }
+
+        public void in_file()
+        {
+            string file_path = "file.txt";
+            if (!File.Exists(file_path))
+                File.Create(file_path).Close();
+            ListViewItem item = new ListViewItem();
+            StreamWriter write = new StreamWriter(file_path);
+            foreach (ListViewItem item2 in listView1.Items)
+            {
+                string str = $"{item2.SubItems[1].Text}_{item2.SubItems[2].Text}_{item2.SubItems[3].Text}";
+                write.WriteLine(str);
+            }
+            write.Close();
+        }
+
+        public void out_file()
+        {
+            string file_path = "file.txt";
+            if (File.Exists(file_path))
+            {
+                StreamReader reader = new StreamReader(file_path);
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    string[] values = line.Split('_');
+                    if (values.Length == 3)
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.SubItems.Add(values[0]);
+                        item.SubItems.Add(values[1]);
+                        item.SubItems.Add(values[2]);
+                        DateTime date = DateTime.Parse(item.SubItems[3].Text);
+                        if (item.SubItems[2].Text == "Сделано")
+                        {
+                            item.Checked = true;
+                            item.BackColor = Color.CornflowerBlue;
+                        }
+                        else if (DateTime.Now <= date) { 
+                            item.SubItems[2].Text = "В процессе";
+                            item.BackColor = Color.LightGray;
+                        }
+                        else if (DateTime.Now > date)
+                        {
+                            item.SubItems[2].Text = "Просроченно";
+                            item.BackColor = Color.Tomato;
+                        }
+
+                        listView1.Items.Add(item);
+                    }
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            in_file();
+        }
+    }
+    
+}
